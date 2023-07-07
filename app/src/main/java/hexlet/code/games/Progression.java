53f449c0e7ce89b52930b372ac7096628db9382b
Daniil Martynov <DaniilDeFacto@yandex.ru>
2023-07-07 16:17:08 +0300

    Fix Progression.java

--- app/src/main/java/hexlet/code/games/Progression.java
+++ app/src/main/java/hexlet/code/games/Progression.java
@@ -4,8 +4,8 @@ import hexlet.code.Engine;
 
 public class Progression {
     public static final int ROUNDS_COUNT = 3;
-    public static final int NUMBER_RANGE = 100;
-    public static final int NUMBERS_COUNT = 10;
+    public static final int NUMBER_RANGE = 30;
+    public static final int NUMBERS_COUNT = 9;
 
     public static void startGame(String userName) {
         System.out.println("What number is missing in the progression?");
@@ -14,8 +14,8 @@ public class Progression {
             int step = (int) (Math.random() * NUMBER_RANGE) + 1;
             String task = Integer.toString(startNumber);
             String correctAnswer = "";
-            int gap = (int) (Math.random() * NUMBERS_COUNT) + 1;
-            for (var j = 1; j < NUMBERS_COUNT; j++) {
+            int gap = (int) (Math.random() * NUMBERS_COUNT);
+            for (var j = 0; j < NUMBERS_COUNT; j++) {
                 startNumber += step;
                 if (j == gap) {
                     task += " " + "..";
